c978d7af93560465934314d662879935281a0c4d
myaooooo98 <yaoer0505@gmail.com>
2023-06-16 18:31:56 +0800

    collecting required elements

---
+++ memoryGame/app.js
@@ -0,0 +1,26 @@
+const cardArray = [
+    {
+        name: 'elephant',
+        img: 'images/elephant.jpg',
+    },
+    {
+        name: 'giraffe',
+        img: 'images/giraffe.jpg',
+    },
+    {
+        name: 'koala',
+        img: 'images/koala.jpg',
+    },
+    {
+        name: 'monkey',
+        img: 'images/monkey.jpg',
+    },
+    {
+        name: 'rhino',
+        img: 'images/rhino.jpg',
+    },
+    {
+        name: 'tiger',
+        img: 'images/tiger.jpg',
+    },
+]
