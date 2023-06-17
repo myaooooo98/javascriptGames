1f974d4272129c0725c546efa3561e14b8578d8b
myaooooo98 <yaoer0505@gmail.com>
2023-06-17 12:37:38 +0800

    adding grid

--- memoryGame/app.js
+++ memoryGame/app.js
@@ -23,4 +23,41 @@ const cardArray = [
         name: 'tiger',
         img: 'images/tiger.jpg',
     },
-]
+    {
+        name: 'elephant',
+        img: 'images/elephant.jpg',
+    },
+    {
+        name: 'giraffe',
+        img: 'images/giraffe.jpg',
+    },
+    {
+        name: 'koala',
+        img: 'images/koala.jpg',
+    },
+    {
+        name: 'monkey',
+        img: 'images/monkey.jpg',
+    },
+    {
+        name: 'rhino',
+        img: 'images/rhino.jpg',
+    },
+    {
+        name: 'tiger',
+        img: 'images/tiger.jpg',
+    },
+];
+
+const grid = document.getElementById('grid');
+
+function createBoard() {
+    cardArray.forEach(ele => {
+        const card = document.createElement('img');
+        card.setAttribute('src', 'images/blank.jpg');
+        card.setAttribute('data-id', cardArray.indexOf(card));
+        grid.appendChild(card);
+    });
+}
+
+createBoard();
