5fff805165b490959ff1920a84a887bdf2bb4290
myaooooo98 <yaoer0505@gmail.com>
2023-06-21 17:54:53 +0800

    add timer, moves counter and records

--- memoryGame/app.js
+++ memoryGame/app.js
@@ -50,13 +50,21 @@ const cardArray = [
 ];
 
 const grid = document.getElementById('grid');
-const resultDisplay = document.querySelector('#result');
 const start = document.getElementById('start');
 const content = document.querySelector('.content');
 const end = document.querySelector('.end');
 const restart = document.getElementById('restart');
+const movesDisplay = document.querySelector('.moves');
+const timerDisplay = document.querySelector('.timer');
+const recordList = document.getElementById('recordList');
+const records = JSON.parse(localStorage.getItem('records')) || [];
+
 let cardsChosen = [];
-let score = 0;
+let moves = 0;
+let match = 0;
+let time;
+let totalTime = 0;
+let name;
 
 //shuffle the card
 cardArray.sort(() => 0.5 - Math.random());
@@ -91,28 +99,78 @@ function checkMatch() {
     let soundEffect = new sound('./sound/correct.mp3');
     let winnerSound = new sound('./sound/won.mp3');
 
+    moves++
+    display(movesDisplay, moves);
+
     if (cardsChosen[0].name === cardsChosen[1].name) {
         soundEffect.play();
         cards[cardsChosen[0].cardId].style['animation-name'] = 'match';
         cards[cardsChosen[1].cardId].style['animation-name'] = 'match';
+        cards[cardsChosen[0].cardId].classList.add('disabled');
+        cards[cardsChosen[1].cardId].classList.add('disabled');
         cards[cardsChosen[0].cardId].removeEventListener('click', flipCard);
         cards[cardsChosen[1].cardId].removeEventListener('click', flipCard);
-        score++;
+        match++;
     } else {
         cards[cardsChosen[0].cardId].setAttribute('src', 'images/blank.jpg');
         cards[cardsChosen[1].cardId].setAttribute('src', 'images/blank.jpg');
     }
 
-    resultDisplay.textContent = score;
-
-    if (score === 6) {
+    if (match === 6) {
+        clearInterval(time);
         setTimeout(function() {
             end.classList.toggle('show');
             winnerSound.play();
         }, 1500);
+
+        const record = {
+            name,
+            moves,
+            totalTime,
+        };
+        records.push(record);
+        let sortedRecords = records.sort((a, b) => {
+            if (a.moves > b.moves) return 1;
+            if (a.moves < b.moves) return -1;
+            if (a.moves === b.moves) {
+                return (a.totalTime > b.totalTime) ? 1 : (a.totalTime < b.totalTime) ? -1 : 0;
+            }
+        });
+        console.log(sortedRecords);
+        populateList(sortedRecords, recordList);
+        localStorage.setItem('records', JSON.stringify(records));
         return;
     }
     cardsChosen = [];
+}
+
+function populateList(plates = [], plateList) {
+    plateList.innerHTML = plates.map((plate, i) => {
+        return `
+            <tr>
+                <td>${i + 1}</td>
+                <td>${plate.name}</td>
+                <td>${plate.moves}</td>
+                <td>${plate.totalTime}</td>
+            </tr>
+        `;
+    }).join('');
+}
+
+function timer() {
+    clearInterval(time);
+    time = setInterval(() => {
+        totalTime++
+        display(timerDisplay, totalTime);
+    }, 1000);
+}
+
+function display(ele, i) {
+    if (ele === movesDisplay) {
+        ele.textContent = `${i} moves`;
+    } else if (ele === timerDisplay) {
+        ele.textContent = `Time: ${i} sec`;
+    }
 }
 
 function sound(src) {
@@ -131,10 +189,13 @@ function sound(src) {
 }
 
 start.addEventListener('click', (e) => {
+    name = prompt("Enter you name!", "player");
     content.classList.toggle('show');
     e.target.closest('.play').style.display = 'none';
-    resultDisplay.textContent = '0';
+    movesDisplay.textContent = `${moves} moves`;
+    timerDisplay.textContent = `Time: ${totalTime} sec`;
     createBoard();
+    timer();
 });
 
 restart.addEventListener('click', () => {
